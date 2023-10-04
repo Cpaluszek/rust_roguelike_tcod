461a5473f690b64f9c652e8ff3fd15007deb2e54
Cpaluszek <paluszek.clement@gmail.com>
2023-10-04 13:58:55 +0200

    add tunnel functions

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use tcod::colors::*;
 use tcod::console::*;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
+use std::cmp;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -73,18 +74,6 @@ struct Game {
     map: Map,
 }
 
-fn make_map() -> Map {
-    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-   
-    // create two rooms
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(50, 15, 10, 15);
-    create_room(room1, &mut map);
-    create_room(room2, &mut map);
-
-    map
-}
-
 #[derive(Clone, Copy, Debug)]
 struct Rect {
     x1: i32,
@@ -101,6 +90,30 @@ impl Rect {
             x2: x + w,
             y2: y + h,
         }
+    }
+}
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+   
+    // create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
+
+    map
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2)) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2)) {
+        map[x as usize][y as usize] = Tile::empty();
     }
 }
 
